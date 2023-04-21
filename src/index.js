07d66b44c6eec073683f0c8d37a9a5d5ebee4172
Misha <mikhail.aleksandrov@nvc-inc.net>
2023-04-21 03:24:06 -0400

    added google tag manager

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css"
 import "./index.css";
 import './i18n';
-import App from "./App";
+import TagManager from 'react-gtm-module'
 import "bootstrap/dist/js/bootstrap.bundle.min"
+import App from "./App";
+
+const tagManagerArgs = {
+    gtmId: process.env.REACT_APP_GTM_ID
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(<App />, document.getElementById("root"));
